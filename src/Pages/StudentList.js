5c0d9839d9e00dc97eef5fc95afe315057ed80f9
hongdotcom <hongdotcom@gmail.com>
2021-03-10 19:58:33 +1300

    updated skill spi

--- src/Pages/StudentList.js
+++ src/Pages/StudentList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { Link } from "react-router-dom";
 const Student = () => {
   useEffect(() => {
     fetchItems();
@@ -31,7 +31,9 @@ const Student = () => {
             return (
               <tr key={index}>
                 <td>
-                  {student.firstname} {student.surname}
+                  <Link to="/student">
+                    {student.firstname} {student.surname}
+                  </Link>
                 </td>
               </tr>
             );
